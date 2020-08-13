397056f2f037666369ffb10cafbcf00c0a63258a
tb95405 <69611134+tb95405@users.noreply.github.com>
2020-08-12 18:32:13 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("I enjoy playing FPS games like Escape from Tarkov");
+		System.out.println("I enjoy programming and building, and that is why I like to do robotics");
+		System.out.println("For college, I would like to go to a UC school like UC Berkely or UC Irvine");
+		System.out.println("I am considering either majoring in Computer Science or some Engineering Science");
+		System.out.println("My favorite ice cream flavor is mango");
+	}
+}
